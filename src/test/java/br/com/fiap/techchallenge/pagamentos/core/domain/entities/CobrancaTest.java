261a637d8233d6b0fc50e034dbc7f1e2959e2a65
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-29 20:59:29 -0300

    Test Coverage

--- src/test/java/br/com/fiap/techchallenge/pagamentos/core/domain/entities/CobrancaTest.java
+++ src/test/java/br/com/fiap/techchallenge/pagamentos/core/domain/entities/CobrancaTest.java
@@ -2,6 +2,9 @@ package br.com.fiap.techchallenge.pagamentos.core.domain.entities;
 
 import br.com.fiap.techchallenge.pagamentos.core.domain.models.enums.StatusEnum;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CobrancaTest {
@@ -9,11 +12,11 @@ class CobrancaTest {
     @Test
     void testCobranca() {
         var QrCode = new QrCode("asddadsapsd=");
-        Cobranca cobranca = new Cobranca(1L, 1L, StatusEnum.PENDENTE, 1150.50, QrCode);
+        Cobranca cobranca = new Cobranca(1L, 1L, StatusEnum.PENDENTE, BigDecimal.TEN, QrCode);
         assertThat(cobranca.getId()).isEqualTo(1L);
         assertThat(cobranca.getPedidoId()).isEqualTo(1L);
         assertThat(cobranca.getStatus()).isEqualTo(StatusEnum.PENDENTE);
-        assertThat(cobranca.getValor()).isEqualTo(1150.50);
+        assertThat(cobranca.getValor()).isEqualTo(BigDecimal.TEN);
         assertThat(cobranca.getQrCode()).isEqualTo(QrCode);
 
     }
